88b171fffcebad439b5f06633a07ea2436c6dc75
raikojima <duyvo761@gmail.com>
2023-03-20 22:34:50 +0700

    Setup project

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { Typographies } from './Source/Constants/Typographies';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
@@ -62,34 +63,12 @@ const App  = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+     <Text style={Typographies.h1}>H1</Text>
+     <Text style={Typographies.h2}>H1</Text>
+     <Text style={Typographies.h3}>H1</Text>
+     <Text style={Typographies.h4}>H1</Text>
+     <Text style={Typographies.h5}>H1</Text>
+     <Text style={Typographies.h6}>H1</Text>
     </SafeAreaView>
   );
 };
